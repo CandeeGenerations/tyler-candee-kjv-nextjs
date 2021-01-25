c0c5db998ce05dc1db0ca32eeea195c5e0486828
Tyler Candee <tyler@candeegenerations.com>
2021-01-25 09:50:58 -0500

    fix: added title to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,10 +2,13 @@ import Layout from '../components/layout'
 import Blockquote from '../components/postComponents/blockquote'
 import Headings from '../components/postComponents/headings'
 import ButtonLink from '../components/buttonLink'
+import {Helmet} from 'react-helmet'
+import {siteTitle} from '../helpers/contants'
 
 const FourOhFour = () => {
   return (
     <Layout>
+      <Helmet title={`404 | ${siteTitle}`} />
       <div className="mt-24">
         <Headings.h1>Jeremiah 29:13 says...</Headings.h1>
 
