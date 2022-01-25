70924f43f97597dd0fb999b86a2c8f99dfc5c358
Tyler Candee <tyler@candeegenerations.com>
2022-01-25 09:33:13 -0500

    chore: deploy updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   content: ['./pages/**/*.tsx', './components/**/*.tsx'],
   theme: {
-    extend: {},
-    fontFamily: {
-      body: '-apple-system, BlinkMacSystemFont, "San Francisco", "Helvetica Neue", Helvetica, Ubuntu, Roboto, Noto, "Segoe UI", Arial, sans-serif',
-      serif: "'Merriweather', Georgia, serif",
+    extend: {
+      fontFamily: {
+        body: '-apple-system, BlinkMacSystemFont, "San Francisco", "Helvetica Neue", Helvetica, Ubuntu, Roboto, Noto, "Segoe UI", Arial, sans-serif',
+        serif: "'Merriweather', Georgia, serif",
+      },
     },
   },
   plugins: [],
